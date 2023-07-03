594a28211c7a620ce9c956629c9de78260d08dae
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-07-03 19:05:19 +0200

    feat: add Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles?: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onMilesChange(event: any) {
+    const value = event.target.value;
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(event: any) {
+    const value = event.target.value;
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(event: any) {
+    const value = event.target.value;
+    this.name = value;
+  }
+
+  onDateChange(event: any) {
+    const value = event.target.value;
+    this.date = value;
+  }
+
+  onAmountChange(event: any) {
+    const value = event.target.value;
+    this.amount = parseFloat(value);
+  }
 }
